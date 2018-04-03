e987ade4acbcac3d2e0f0f2bb76d1eb37c454c5c
harvey <harveyaot@gmail.com>
2018-04-03 13:16:08 +0800

    more detailed usage example

--- train/utils.py
+++ train/utils.py
@@ -4,8 +4,8 @@ MONGODB_SERVER = "13.75.105.50"
 #MONGODB_SERVER = "localhost"                                                                   
 MONGODB_PORT = 27018                                                                          
 MONGODB_DB = "alphataibai"                                                                      
-MONGODB_USER = "" # ask project owner for this
-MONGODB_PWD = ""  # ask project owner for his                              
+MONGODB_USER = "l*" # ask project owner to update this
+MONGODB_PWD = "z*"  # ask project owner to update this                              
 MONGODB_COLLECTION_AUTHOR = "author"   
 MONGODB_COLLECTION_SHIWEN = "shiwen"   
 
@@ -24,7 +24,13 @@ def get_mongodb():
 def get_doc_count():
     db = get_mongodb()
     shiwen = db[MONGODB_COLLECTION_SHIWEN]
+    tmp_one = shiwen.find_one()
     print shiwen.count()
+    print tmp_one.keys()
+    print tmp_one['title']
+    print tmp_one['body']
+    print tmp_one['author']
+    print tmp_one['like']
 
 if __name__ == "__main__":
     get_doc_count()
