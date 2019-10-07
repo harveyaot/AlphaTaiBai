ec7ae7a49331735b62fbfb245e69cafc61ba4336
Wei <harveyaot@gmail.com>
2019-10-07 20:35:56 +0800

    add logging function

--- docker/sem_search/app/flask_search.py
+++ docker/sem_search/app/flask_search.py
@@ -1,7 +1,13 @@
+import logging
+from logging.config import fileConfig
+
 from flask import Flask
 from flask import request
 import json
 from vector_es_search import *
+
+fileConfig('conf/logging_config.ini')
+logger = logging.getLogger('search')
 
 app = Flask(__name__)
 
@@ -10,10 +16,28 @@ def sem_search():
     sent = request.json
     vec = get_vec4query(sent)
     resp = search_by_vec(vec)
+    logger.info(log_request_record(sent, resp))
     if resp.ok:
         return resp.json()
     else:
         return resp.text
+
+def log_request_record(sent, resp):
+    resp = format_resp(resp)
+    record = "\"{}\"\t{}".format(sent, resp)
+    return record
+
+def format_resp(resp):
+    if resp and "hits" in resp and "hits" in resp['hits']:
+        hits = resp['hits']['hits']
+        res = []
+        for hit in hits:
+            sent = hit["_source"]["sent"]
+            score = hit['_score']
+            res.append("{}|{0:.2f}".format(sent,score))
+        return "$".join(res)
+    else:
+        return None
 
 if __name__ == "__main__":
     app.run()
--- scripts/test_flask.py
+++ scripts/test_flask.py
@@ -1,10 +1,24 @@
 import requests
 import json
-data = ['asdf', 'asdf']
-#url = "http://localhost:5000/api/emb/v1"
-hostname= "localhost"
-url = "http://{}:5000/api/emb/v1".format(hostname)
-headers = {'content-type': 'application/json'}
-resp = requests.post(url, json.dumps(data), headers=headers)
-d = resp.json()
-print(len(d), len(d[0]))
+def test_emb():
+    data = ['asdf', 'asdf']
+    #url = "http://localhost:5000/api/emb/v1"
+    hostname= "localhost"
+    url = "http://{}:5000/api/emb/v1".format(hostname)
+    headers = {'content-type': 'application/json'}
+    resp = requests.post(url, json.dumps(data), headers=headers)
+    d = resp.json()
+    print(len(d), len(d[0]))
+
+def test_search():
+    sent = '当爱已成往事'
+    #url = "http://localhost:5000/api/emb/v1"
+    hostname= "localhost"
+    url = "http://{}:8600/api/sem_search/v1".format(hostname)
+    headers = {'content-type': 'application/json'}
+    resp = requests.post(url, json.dumps(sent), headers=headers)
+    d = resp.json()
+    print(d)
+
+if __name__ == "__main__":
+    test_search()
