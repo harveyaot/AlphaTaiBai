3d3124e2b66d00776fae3301e1b818355369e1f0
Wei He <hewe@microsoft.com>
2021-12-23 08:54:59 +0800

    generic image crawler on Azure functions

---
+++ azure_functions/Utils.cs
@@ -0,0 +1,47 @@
+namespace AlphaTaibai.Utilities
+{
+    using System;
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    public static class Helper 
+    {
+        public static Image ResizeImage(Image imgToResize, Size destinationSize)
+        {
+            var originalWidth = imgToResize.Width;
+            var originalHeight = imgToResize.Height;
+
+            //how many units are there to make the original length
+            var hRatio = (float)originalHeight / destinationSize.Height;
+            var wRatio = (float)originalWidth / destinationSize.Width;
+
+            //get the shorter side
+            var ratio = Math.Min(hRatio, wRatio);
+
+            var hScale = Convert.ToInt32(destinationSize.Height * ratio);
+            var wScale = Convert.ToInt32(destinationSize.Width * ratio);
+
+            //start cropping from the center
+            var startX = (originalWidth - wScale) / 2;
+            var startY = (originalHeight - hScale) / 2;
+
+            //crop the image from the specified location and size
+            var sourceRectangle = new Rectangle(startX, startY, wScale, hScale);
+
+            //the future size of the image
+            var bitmap = new Bitmap(destinationSize.Width, destinationSize.Height);
+
+            //fill-in the whole bitmap
+            var destinationRectangle = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            //generate the new image
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(imgToResize, destinationRectangle, sourceRectangle, GraphicsUnit.Pixel);
+            }
+
+            return bitmap;
+
+        }
+    }
+}
---
+++ azure_functions/models.cs
@@ -0,0 +1,31 @@
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace AlphaTaibai.Models{
+
+    public class ImageToCrawlMsg {
+        [JsonProperty("image_url")]
+        public string ImageUrl {get; set;}
+        [JsonProperty("add_string_info")]
+        public Dictionary<string, string> AddStringInfo {get; set;}
+        public Dictionary<string, int> AddIntInfo {get; set;}
+    }
+    public class SavedImageInfo
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Container { get; set; }
+        public string BlobPath { get; set; }
+        public string ImageUrl { get; set; }
+        public long ImageSize { get; set; }
+        public int? Width {get; set;}
+        public int? Height {get; set;}
+        public Dictionary<string, string> AddStringInfo {get; set;}
+        // include sourceurl, sourcecode, wpmediakd, width
+        public Dictionary<string, int> AddIntInfo {get; set;}
+        public Dictionary<string, List<int>> AddEmbInfo {get; set;}
+        public long UploadTimeStamp { get; set; }
+    }
+}
